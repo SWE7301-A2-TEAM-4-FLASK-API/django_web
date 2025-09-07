9d5acf8884ab2db5eb1b283efa6ee535e9735615
Your Full Name <nachyjoe@gmail.com>
2025-09-07 21:59:20 +0100

    updated the files in subscription template

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from products.models import Product
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    started_at = models.DateField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    renewal_date = models.DateField(null=True, blank=True)
+    payment_method = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} → {self.product.name}'
